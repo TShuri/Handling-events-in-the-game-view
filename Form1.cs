830deb12ab54504f767b029ff348823917ea170a
TShuri <andr.xti@gmail.com>
2022-11-21 12:37:05 +0800

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+using Event_Handling.Objects;
+
+namespace Event_Handling
+{
+    public partial class Form1 : Form
+    {
+        MyRectangle myRect;
+        public Form1()
+        {
+            InitializeComponent();
+            myRect = new MyRectangle(0, 0, 0);
+        }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.Clear(Color.White);
+            /*
+            g.FillRectangle(new SolidBrush(Color.Yellow), 200, 100, 50, 30);
+            g.DrawRectangle(new Pen(Color.Red, 2), 200, 100, 50, 30);
+            */
+            myRect.Render(g);
+        }
+    }
+}
---
+++ Objects/BaseObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Handling.Objects
+{
+    class BaseObject
+    {
+        public float X;
+        public float Y;
+        public float Angle;
+
+        public BaseObject(float x, float y, float angle) // Конструктор    
+        {
+            X = x;
+            Y = y;
+            Angle = angle;
+        }
+
+        public virtual void Render(Graphics g)
+        {
+            // Пока пусто
+        }
+    }
+}
